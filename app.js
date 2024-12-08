1d5a9af20fc20f7fd823d82f305d66cc4bfbff28
NguyenM1nhKho1 <khoi.nguyenminh2022@hcmut.edu.vn>
2024-12-08 23:13:25 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
     res.send("Hello Chat");
 });
 
-const PORT = 3000;
+const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 }); 
